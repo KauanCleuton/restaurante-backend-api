7829ed7c497a532b81cfc3e601b2af6184ea4a9b
kauanbrtech <kauandasilva@brtech.dev>
2024-02-11 15:49:13 -0300

    deploy

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,34 +1,24 @@
 import prisma from "../config/database";
 import IUser from "../types/user.type";
 
-
 const listUsers = async () => {
-    try {
-        const users = await prisma.user.findMany()
-        return users
-    } catch (error) {
-        console.log('Server internal error!', error)
-        throw new Error('Server internal error')
-    }
-}
+    const users = await prisma.user.findMany();
+    return users;
+};
+
 const register = async (data: IUser) => {
-    try {
-        const registerUser = await prisma.user.create({
-            data: {
-                email: data.email,
-                name: data.name,
-                password: data.password,
-                created_at: new Date()
-            }
-        })
-        return registerUser
-    } catch (error) {
-        console.log('Server internal error!', error)
-        throw new Error('Server internal error')
-    }
-}
+    const registerUser = await prisma.user.create({
+        data: {
+            email: data.email,
+            name: data.name,
+            password: data.password,
+            created_at: new Date()
+        }
+    });
+    return registerUser;
+};
 
 export default {
     register,
     listUsers
-}
+};
